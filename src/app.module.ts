523fb66e909cff58301d12e75a96c15a527f41d3
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-07-17 14:07:19 +0900

    build: DB 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { envVariables } from './common/env.const';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    // DB 설정
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(envVariables.dbHost),
+        port: configService.get<number>(envVariables.dbPort),
+        username: configService.get<string>(envVariables.dbUsername),
+        password: configService.get<string>(envVariables.dbPassword),
+        database: configService.get<string>(envVariables.dbDatabase),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        // synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/env.const.ts
@@ -0,0 +1,14 @@
+// DB
+const dbHost = 'DB_HOST';
+const dbDatabase = 'DB_DATABASE';
+const dbPort = 'DB_PORT';
+const dbUsername = 'DB_USERNAME';
+const dbPassword = 'DB_PASSWORD';
+
+export const envVariables = {
+  dbHost,
+  dbDatabase,
+  dbPassword,
+  dbPort,
+  dbUsername,
+};
